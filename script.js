56c670c2d47b7fca9f37b6fba5666bf080a39eb7
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-19 14:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+    let x=document.createElement("tr");
+	let a=document.createElement("td");
+	let b=document.createElement("td");
+	a.innerText="New Cell1";
+	b.innerText="New Cell2";
+	x.appendChild(a);
+	x.appendChild(b);
+	document.getElementById("sampleTable").prepend(x)
   
 }
